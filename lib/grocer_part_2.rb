ec3a001d0229fdc65b6c2875f66a1e620ff8019b
Anh Vuong <vuonga1103@yahoo.com>
2020-06-11 00:52:13 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,38 @@
 require_relative './part_1_solution.rb'
+require "pry"
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
+  # Loop through each coupon in coupons array
+  coupons.each do |coupon|
+    
+    # Create a variable to store the coupon item
+    coupon_item = find_item_by_name_in_collection(coupon[:item], cart)
+    
+    # Create a variable that tells us if the coupon item exists in the cart, set to boolean value of coupon_item (if find_item_in_cart function results to nil, then coupon_item_is_in_cart will be false)
+    coupon_item_is_in_cart = !!coupon_item
+    
+    # If the coupon item is in the cart and we have enough of the items to use the coupon, then push the new hash into cart
+    if coupon_item_is_in_cart && coupon_item[:count] >= coupon[:num]
+      
+      discounted_item_hash = {}
+      discounted_item_hash[:item] = coupon_item[:item] + " W/COUPON"
+      discounted_item_hash[:price] = coupon[:cost] / coupon[:num]
+      discounted_item_hash[:clearance] = coupon_item[:clearance]
+      discounted_item_hash[:count] = coupon[:num]
+      
+      cart << discounted_item_hash
+      
+      # Update the cart's item
+      coupon_item[:count] -= coupon[:num]
+    end
+  end
+  
+  # Return cart
+  cart
 end
 
 def apply_clearance(cart)
